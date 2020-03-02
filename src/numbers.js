9079c13c7d30c778e08372ed935b43a9c90f2cd8
aar.56aint <aaron.hays_7@yahoo.com>
2020-03-02 20:59:32 +0000

    first commit for sofia

--- src/numbers.js
+++ src/numbers.js
@@ -1,45 +1,48 @@
 const add = (a, b) => {
-  // your code here
+  return a + b;
 };
 
+//module.exports = add;
+
 const subtract = (a, b) => {
-  // your code here
+  // your code here//return a - b;
 };
 
 const multiply = (a, b) => {
-  // your code here
+  // your code here//return a * b;
 };
 
 const divide = (a, b) => {
-  // your code here
+  // your code here//return a / b;
 };
 
 const power = (a, b) => {
-  // your code here
+  // your code here//return a ** b;
 };
 
 const round = a => {
-  // your code here
+  // your code here//return Math.round(a);
 };
 
 const roundUp = a => {
-  // your code here
+  // your code here//return Math.ceil(a);
 };
 
 const roundDown = a => {
-  // your code here
+  // your code here//return Math.floor(a);
+  
 };
 
 const absolute = a => {
-  // your code here
+  // your code here//return Math.abs(a);
 };
 
 const quotient = (a, b) => {
-  // your code here
+  // your code here//return Math.trunc(a / b);
 };
 
 const remainder = (a, b) => {
-  // your code here
+  // your code here//return a % b;
 };
 
 module.exports = {
