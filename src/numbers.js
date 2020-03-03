88e3466432a79c60188abee48ea67a29ebff80ad
Sofia <sofia.dionisio94@gmail.com>
2020-03-03 10:55:20 +0000

    numbers done

--- src/numbers.js
+++ src/numbers.js
@@ -5,44 +5,43 @@ const add = (a, b) => {
 //module.exports = add;
 
 const subtract = (a, b) => {
-  // your code here//return a - b;
+  return a - b;
 };
 
 const multiply = (a, b) => {
-  // your code here//return a * b;
+  return a * b;
 };
 
 const divide = (a, b) => {
-  // your code here//return a / b;
+  return a / b;
 };
 
 const power = (a, b) => {
-  // your code here//return a ** b;
+  return a ** b;
 };
 
 const round = a => {
-  // your code here//return Math.round(a);
+  return Math.round(a);
 };
 
 const roundUp = a => {
-  // your code here//return Math.ceil(a);
+  return Math.ceil(a);
 };
 
 const roundDown = a => {
-  // your code here//return Math.floor(a);
-  
+  return Math.floor(a);
 };
 
 const absolute = a => {
-  // your code here//return Math.abs(a);
+  return Math.abs(a);
 };
 
 const quotient = (a, b) => {
-  // your code here//return Math.trunc(a / b);
+  return Math.trunc(a / b);
 };
 
 const remainder = (a, b) => {
-  // your code here//return a % b;
+  return a % b;
 };
 
 module.exports = {
