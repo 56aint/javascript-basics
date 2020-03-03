64aa3536c8acc9aec9459b92b193811085c5d92b
aar.56aint <aaron.hays_7@yahoo.com>
2020-03-03 10:41:51 +0000

    2nd number commit

--- src/numbers.js
+++ src/numbers.js
@@ -2,47 +2,54 @@ const add = (a, b) => {
   return a + b;
 };
 
-//module.exports = add;
-
 const subtract = (a, b) => {
-  // your code here//return a - b;
+  // your code here
+  return a - b;
 };
 
 const multiply = (a, b) => {
-  // your code here//return a * b;
+  // your code here
+  return a * b;
 };
 
 const divide = (a, b) => {
-  // your code here//return a / b;
+  // your code here
+  return a / b;
 };
 
 const power = (a, b) => {
-  // your code here//return a ** b;
+  // your code here
+  return a ** b;
 };
 
 const round = a => {
-  // your code here//return Math.round(a);
+  // your code here
+  return Math.round(a);
 };
 
 const roundUp = a => {
-  // your code here//return Math.ceil(a);
+  // your code herex
+  return Math.ceil(a);
 };
 
 const roundDown = a => {
-  // your code here//return Math.floor(a);
-  
+  // your code here
+  return Math.floor(a);
 };
 
 const absolute = a => {
-  // your code here//return Math.abs(a);
+  // your code here
+  return Math.abs(a);
 };
 
 const quotient = (a, b) => {
-  // your code here//return Math.trunc(a / b);
+  // your code here
+  return Math.trunc(a / b);
 };
 
 const remainder = (a, b) => {
-  // your code here//return a % b;
+  // your code here
+  return a % b;
 };
 
 module.exports = {
