3fda0a2d30476e8d4921b0d622fc91de63670eac
Sofia <sofia.dionisio94@gmail.com>
2020-03-05 14:46:24 +0000

    changes made in arrays

--- src/arrays.js
+++ src/arrays.js
@@ -40,31 +40,54 @@ const uppercaseWordsInArray = strings => {
 
 const reverseWordsInArray = strings => {
   // your code here
-  return strings.map(rev => rev.reverse());
+  return strings.map(rev => rev.split("").reverse().join(""));
 };
 
 const onlyEven = numbers => {
   // your code here
+  return numbers.filter(num => num % 2 === 0);
 };
 
 const removeNthElement2 = (index, array) => {
   // your code here
+  
 };
 
 const elementsStartingWithAVowel = strings => {
   // your code here
+  const results = [];
+  for (let i = 0; i < strings.length; i++) {
+  if ((strings[i].startsWith('a')) || 
+      (strings[i].startsWith('e')) || 
+      (strings[i].startsWith('i')) || 
+      (strings[i].startsWith('o')) || 
+      (strings[i].startsWith('u')) ||
+      (strings[i].startsWith('A')) || 
+      (strings[i].startsWith('E')) || 
+      (strings[i].startsWith('I')) || 
+      (strings[i].startsWith('O')) || 
+      (strings[i].startsWith('U'))) {
+        results.push(strings[i])
+      }
+ }
+ return results;
 };
 
 const removeSpaces = string => {
   // your code here
+  return string.replace(/\s/g,'');
 };
 
 const sumNumbers = numbers => {
   // your code here
+  return numbers.reduce((a, b) => a + b, 0);
 };
 
 const sortByLastLetter = strings => {
   // your code here
+  const reversed = reverseWordsInArray(strings);
+  reversed.sort();
+  return reverseWordsInArray(reversed);
 };
 
 module.exports = {
